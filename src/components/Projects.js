9f8cb24c92bb80da11066f6829064ca1fc022201
CMack32 <61818457+CMack32@users.noreply.github.com>
2022-11-07 14:14:22 -0500

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center",
+    paddingBottom: "11px",
+  },
+  bigSpace: {
+    marginTop: "5rem",
+    width: "20%",
+    textAlign: "left",
+  },
+  littleSpace: {
+    marginTop: "2.5rem",
+    textAlign: "left",
+  },
+});
+
+function Projects() {
+  const classes = styles();
+  return (
+    <div className={classes.wrapper}>
+      <Typography variant="h2" className={classes.bigSpace} color="primary">
+        Under Construction.
+      </Typography>
+    </div>
+  );
+}
+
+export default Projects;
